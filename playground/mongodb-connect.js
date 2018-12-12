1f8f11399b5d84459f1cf3be6780a2460c5bdf59
Rodrigo <rodrigo@zooft.company>
2018-12-12 18:32:43 -0300

    set up app for Heroku

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -4,7 +4,7 @@ const {MongoClient, ObjectID} = require('mongodb');
 var obj = new ObjectID();
 console.log(obj);
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+MongoClient.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err){
     return console.log('Unable to connect to the database server');
   }
--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -54,8 +54,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 if(!module.parent){
-  app.listen(3000, () => {
-    console.log('The server is running in 3000');
+  app.listen(PORT, () => {
+    console.log(`The server is running in the port: ${PORT}`);
   });
 }
 
