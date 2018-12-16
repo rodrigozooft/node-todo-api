d6b6da541aa9c91e32aa8d16ca6f14687388fec9
Rodrigo <rodrigo@zooft.company>
2018-12-16 18:17:11 -0300

    implementing the patch method to update todos text and completed status

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -70,6 +71,32 @@ app.delete('/todos/:id', (req, res) => {
   } else {
     res.status(404).send('It is not a valid Id');
   }
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo){
+      return res.status(400).send();
+    }
+    res.send({todo});
+  }).catch((err) => {
+    res.status(400).send();
+  })
+
 });
 
 if(!module.parent){
