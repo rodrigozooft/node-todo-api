33e114ce6573e6bacab418b7e635c810e217b064
Rodrigo <rodrigo@zooft.company>
2018-12-26 19:11:51 -0300

    adding bcrypt to hash passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,20 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) =>{
+//   bcrypt.hash(password, salt, (err, hash) =>{
+//     console.log('Hash and Salt : ', hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$TpT8sSHPd3YeMoh0.VzfgOl85HR7dO7hwLDr9iqvoPbBCLgZ.ZdNO';
+
+bcrypt.compare(password, hashedPassword, (err, res) =>{
+  console.log(res);
+});
 
 var data = {
   id: 10
